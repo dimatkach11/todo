1bcfb871b6d4e7ab9c0d7636551decf6939e538d
dimatkach11 <dimitritkach@gmail.com>
2021-02-10 12:33:13 +0100

    djangorestframwork added serializer to model Hero

---
+++ todo/appTodo/serializers.py
@@ -0,0 +1,16 @@
+# serializers.py
+
+# In this file, we need to:
+# Import the Hero model
+# Import the REST Framework serializer
+# Create a new class that links the Hero with its serializer
+
+
+from rest_framework import serializers
+
+from .models import Hero
+
+class HeroSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Hero
+        fields = ('name', 'alias')
--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'appTodo.apps.ApptodoConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
