9f515a23a37069e63e1085c62fa14692e648258e
dimatkach11 <dimitritkach@gmail.com>
2021-02-10 12:37:56 +0100

    added HeroViewSet for display the data

--- todo/appTodo/views.py
+++ todo/appTodo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# we need to:
+# Query the database for all heroes
+# Pass that database queryset into the serializer we just created, so that it gets converted into JSON and rendered
+
+from rest_framework import views
+
+from .serializers import HeroSerializer
+from .models import Hero
+
+class HeroViewSet(viewsets.ModelViewSet):
+    queryset = Hero.objects.all().order_by('name')
+    serializer_class = HeroSerializer
